86771e4a90b982da5aed1a05d6dc561e7c679dcf
garveet jain <91081271+Jaingarveet@users.noreply.github.com>
2026-07-17 20:27:17 +0200

    LLM and tavily internal error handling strategy
    Updated the graph to handle internal timeout,ratelimit,connection errors from openai and tavily client and implemented a retry strategy for that

--- graph.py
+++ graph.py
@@ -1,20 +1,64 @@
-# import nodes:
-from nodes_and_edges import input_structuring_node, analyst_creator, HITL, conditional_edge_HITL, generate_and_ask_question, finish_convo, expert_search_query, web_search, answer_question, collect_interviews, content_compiler, latex_compiler, commit_to_overleaf
-
 # import required states: 
 from states import UserSideInput, SingleInterviewState
 # general imports: 
 from langgraph.graph import StateGraph, START, END
-from langchain.chat_models import init_chat_model
-from langgraph.checkpoint.memory import InMemorySaver
 
+from openai import RateLimitError, APIConnectionError, APITimeoutError
+from typing import Callable
+
+# import nodes:
+from nodes_and_edges import (input_structuring_node, analyst_creator, HITL, conditional_edge_HITL, generate_and_ask_question, finish_convo, expert_search_query, web_search, answer_question, collect_interviews, content_compiler, latex_compiler, commit_to_overleaf)
+
+from langgraph.types import RetryPolicy
+
+def LLM_retry_error_filter(error: Exception) -> bool:
+    """Only retry network/ratelimit errors"""
+    return isinstance(error,(RateLimitError, APIConnectionError, APITimeoutError))
+
+def Tavily_retry_error_filter(error: Exception) -> bool:
+    """Only retry network/api errors"""
+    return isinstance(error,(ConnectionError, TimeoutError))
+
+LLM_nodes_retry_policy = RetryPolicy(
+    initial_interval = 0.5,
+    backoff_factor = 2.0,
+    max_attempts = 3,
+    max_interval = 128,
+    jitter = False, # no need to add random variations in retries since no parallel nodes are retrying at same time
+    retry_on = LLM_retry_error_filter
+)
+
+Tavily_search_node_retry_policy= RetryPolicy(
+    initial_interval = 0.5,
+    backoff_factor = 2.0,
+    max_attempts = 3,
+    max_interval = 128,
+    jitter = False, 
+    retry_on = Tavily_retry_error_filter
+)
+
+
+def add_LLM_node(builder: StateGraph, name_of_node: str, node_funciton: Callable) -> StateGraph:
+    """ Returns a node to graph builder with LLM_nodes_retry_policy as retry policy"""
+    
+    return builder.add_node(name_of_node, node_funciton, retry = LLM_nodes_retry_policy)
+
+def add_Tavily_node(builder: StateGraph, name_of_node: str, node_funciton: Callable) -> StateGraph:
+    
+    """ Returns a node to graph builder with LLM_nodes_retry_policy as retry policy"""
+    
+    return builder.add_node(name_of_node, node_funciton, retry = Tavily_search_node_retry_policy)
+    
 
 interview_sub_graph = StateGraph(SingleInterviewState)
-interview_sub_graph.add_node('analyst',generate_and_ask_question)
-interview_sub_graph.add_node('expert_search', expert_search_query)
-interview_sub_graph.add_node('web_search',web_search)
-interview_sub_graph.add_node('answer_question', answer_question)
-interview_sub_graph.add_node('collect_interviews',collect_interviews)
+
+add_LLM_node(interview_sub_graph, 'analyst' ,generate_and_ask_question)
+add_LLM_node(interview_sub_graph,'expert_search', expert_search_query)
+
+add_Tavily_node(interview_sub_graph,'web_search',web_search)
+
+add_LLM_node(interview_sub_graph,'answer_question', answer_question)
+add_LLM_node(interview_sub_graph,'collect_interviews',collect_interviews)
 
 interview_sub_graph.add_edge(START, 'analyst')
 interview_sub_graph.add_conditional_edges('analyst', finish_convo, ['collect_interviews','expert_search'])
@@ -25,13 +69,17 @@ interview_sub_graph.add_edge('collect_interviews',END)
 
 
 research_graph_builder = StateGraph(UserSideInput)
-research_graph_builder.add_node('structure_input', input_structuring_node)
-research_graph_builder.add_node('analyst_creator', analyst_creator)
-research_graph_builder.add_node('HITL', HITL)
+
+add_LLM_node(research_graph_builder,'structure_input', input_structuring_node)
+add_LLM_node(research_graph_builder,'analyst_creator', analyst_creator)
+
+research_graph_builder.add_node('HITL', HITL) # no llm used here
 research_graph_builder.add_node('conduct_interviews', interview_sub_graph.compile())
-research_graph_builder.add_node('content_compiler',content_compiler)
-research_graph_builder.add_node('latex_compiler',latex_compiler)
-research_graph_builder.add_node('commit_to_overleaf',commit_to_overleaf)
+
+add_LLM_node(research_graph_builder,'content_compiler',content_compiler)
+add_LLM_node(research_graph_builder,'latex_compiler',latex_compiler)
+
+research_graph_builder.add_node('commit_to_overleaf',commit_to_overleaf) #runs a bash script manually
                         
 research_graph_builder.add_edge(START, 'structure_input')
 research_graph_builder.add_edge('structure_input', 'analyst_creator')
